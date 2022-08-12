3b734bb90b3fff0708d423222d7cde6bff2d1b70
Bill Huynh <youkaisanx@gmail.com>
2022-08-12 14:10:31 -0500

    Check palindrome with tests

---
+++ Palindrome/Palindrome.Tests.Unit/WordCheckerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Palindrome.Tests.Unit
+{
+    public class WordCheckerTests
+    {
+        [TestCase("obo", ExpectedResult=true)]
+        [TestCase("poop", ExpectedResult=true)]
+        [TestCase("simis", ExpectedResult=true)]
+        public bool IsPalindrome_ShouldReturnTrue_WhenGivenValidPalindrome(string input)
+        {
+            var sut = new WordChecker();
+            var result = sut.IsPalindrome(input);
+
+            return result;
+        }
+    }
+}
---
+++ Palindrome/Palindrome/WordChecker.cs
@@ -0,0 +1,23 @@
+﻿namespace Palindrome
+{
+    public class WordChecker
+    {
+        public bool IsPalindrome(string word)
+        {
+            int half = word.Length / 2;
+
+            int end = word.Length - 1;
+            for(int start = 0; start < half; start++)
+            {
+                if (word[start] != word[end])
+                {
+                    return false;
+                }
+
+                end--;
+            }
+
+            return true;
+        }
+    }
+}
